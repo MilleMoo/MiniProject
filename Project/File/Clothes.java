fb520ce1be3dc0570035b2d6d8aa7d147aaf2021
MilleMoo <118721832+MilleMoo@users.noreply.github.com>
2023-09-10 04:34:42 +0700

    Update Clothes.java

--- Project/File/Clothes.java
+++ Project/File/Clothes.java
@@ -8,9 +8,19 @@ public interface Clothes {
     public void setFabric(int fabric);//แก้ไขชนิดของผ้า
 
     public void setStyle(int Style);//แก้ไขรูปแบบของผ้า
+    public void setFabricCharges(int FabricCharges);
+    public void setSizeCharges(int SizeCharges);
+    public void setOverAllCharges(int OverAllCharges);
 
+    
     public String getSize();//รับค่าขนาดของเสื้อ
     public String getColor();//รับค่าสีของเสื้อ
     public int getFabric();//รับค่าชนิดของผ้า
+
     public int getStyle();//รับค่ารูปแบบของผ้า
+
+    public int getFabricCharges();
+
+    public int getSizeCharges();
+    public int getOverAllCharges();
 }
