d75687f15e642623081bb9a8cc2924a90fe8d588
MilleMoo <118721832+MilleMoo@users.noreply.github.com>
2023-09-10 04:32:41 +0700

    Update MainManu.java
    ทำระบบคิดเงินเสร็จแล้ว เหลือเพิ่มโปรโมชั่น

--- Project/File/MainManu.java
+++ Project/File/MainManu.java
@@ -2,26 +2,28 @@ package Project.File;
 
 import java.util.*;
 
-public class MainManu {
+public abstract class MainManu {
     private double Money;
     private int Index;
     private String Size;
     private String Color;
     private int Fabric;
+    private int FabricCharges;
+    private int SizeCharges;
     private int Select;
     private int count = 0;
     private Scanner sc = new Scanner(System.in);
     private ArrayList<Clothes> Stock = new ArrayList<>();
+    public abstract void Bill();
     private String Style = "--------Manu----------\n1.)TShirt\n2.)Polo\n3.)Jacket\nChoose the style of shirt you want to buy : ";
 
     public MainManu() {
-        MainManu1();
     }
     
     public void MainManu1() {//เลือกสินค้าว่าจะมีลักษณะเป็นยังไง
             System.out.print(Style);
             Clothes Item = null;
-            this.Select = sc.nextInt();
+            this.Select = Integer.parseInt(sc.nextLine());
             switch (this.Select) {
                 case 1:
                     Input();
@@ -51,31 +53,39 @@ public class MainManu {
     private void MainManu2() {//มีไว้แก้ไขหรือเพิ่มสินค้าก่อนคิดเงิน
         System.out.print("\n--------Manu----------\n" +
                 "1.)Choose AntherOne\n" +
-                "2.)EditSize\n" +
-                "3.)EditColor\n" +
-                "4.)EditFabric\n" +
-                "5.)Remove out\n" +
-                "6.)ready to pay\n" +
+                "2.)EditStyle\n" +
+                "3.)EditSize\n" +
+                "4.)EditColor\n" +
+                "5.)EditFabric\n" +
+                "6.)Remove out\n" +
+                "7.)Remove All Clothes in Stock\n"+
+                "8.)ready to pay\n" +
                 "Choose Function : ");
-        switch (sc.nextInt()) {
+        switch (Integer.parseInt(sc.nextLine())) {
             case 1:
-            MainManu1();
+                MainManu1();
                 break;
             case 2:
-                EditSize();
-                break;
+                EditStyle();
+            break;
             case 3:
-                EditColor();
-                break;
+                EditSize();
+            break;
             case 4:
-                EditFabric();
-                break;
+                EditColor();
+            break;
             case 5:
-                Remove();
-                break;
+                EditFabric();
+            break;
             case 6:
-
+                RemoveClothes();
+            break;
+            case 7:
+                RemoveAll();
                 break;
+            case 8:
+                Bill();
+                return;
 
             default:
                 System.out.println("Invalid choice");
@@ -85,22 +95,32 @@ public class MainManu {
     
     
     public void Input() {//รับค่าขนาดเสื้อสีและชนิดของผ้า
-        System.out.print("\nSize A 32\" B 23\" : S\n" +
-                "Size A 34\" B 24\" : M\n" +
-                "Size A 36\" B 25\" : L\n" +
-                "Size A 38\" B 26\" : XL\n" +
+        System.out.print("\nSize A 32\" B 23\" : S + 50Bath\n" +
+                "Size A 34\" B 24\" : M + 100Bath\n" +
+                "Size A 36\" B 25\" : L + 150Bath\n" +
+                "Size A 38\" B 26\" : XL + 200Bath\n" +
                 "Please choose Size : ");
-        sc.nextLine();
         this.Size = sc.nextLine();
+        if(this.Size.equalsIgnoreCase("S") || this.Size.equalsIgnoreCase("M")
+                || this.Size.equalsIgnoreCase("L") || this.Size.equalsIgnoreCase("XL")) {//Check Size select Not pass
+            this.Size = this.Size.toUpperCase();
         System.out.print("\nPlease choose Color : ");
         this.Color = sc.nextLine();
-        System.out.print("\n\n1.) Cotton\n" +
-                "2.) Satin\n" +
-                "3.) Nylon\n" +
+        this.Color = this.Color.toUpperCase();
+        System.out.print("\n\n1.) Cotton + 300Bath\n" +
+                "2.) Satin + 350Bath\n" +
+                "3.) Nylon + 400Bath\n" +
                 "Please choose Fabric : ");
-        this.Fabric = sc.nextInt();
-        sc.nextLine();
+        this.Fabric = Integer.parseInt(sc.nextLine());
+        if(this.Fabric >= 4 || this.Fabric <=  0){//Check Fabric select Not pass
+            System.out.println("You input Fabric wrong please input one more time");
+                Input();
+            }
         this.count++;
+        } else {
+            System.out.println("You input wrong Size please input one more time");
+            Input();
+        }
     }
     
     public String ShowStyle(int Select) {//แปลงตัวเลขเป็นตัวอักษรเพื่อที่จะได้รู้รูปแบบของผ้า
@@ -145,53 +165,173 @@ public class MainManu {
 
     public void ShowValueInArrayList() {//แสดงค่าทั้งหมดในArraylist
         System.out.println("Clothes in your the List: ");
+        CheckBill();
         for (int i = 0; i < Stock.size(); i++) {
             Clothes item = Stock.get(i);
-            System.out.println("Clothes: " + count);
+            System.out.println("Clothes: " + count + " Price: " + item.getOverAllCharges() + " Bath");
             System.out.println("Style: " + ShowStyle(item.getStyle()));
-            System.out.println("Size: " + item.getSize());
+            System.out.println("Size: " + item.getSize() + " Price: " + item.getSizeCharges() + " Bath");
             System.out.println("Color: " + item.getColor());
-            System.out.println("Fabric: " + ShowFabric(item.getFabric()) + "\n");
+            System.out.println("Fabric: " + ShowFabric(item.getFabric()) + " Price: " + item.getFabricCharges()+ " Bath" + "\n");
         }
+        System.out.println("total price : "+this.Money);
     }
     
-    public void EditSize() {
-        sc.nextLine();
+    public void EditSize() {//Edit Size in Index
+        System.out.println("\nSize A 32\" B 23\" : S +50Bath\n" +
+                "Size A 34\" B 24\" : M +100Bath\n" +
+                "Size A 36\" B 25\" : L +150Bath\n" +
+                "Size A 38\" B 26\" : XL +2000Bath\n");
         System.out.print("Please input Size : ");
         this.Size = sc.nextLine();
+        if(this.Size.equalsIgnoreCase("S") || this.Size.equalsIgnoreCase("M")
+        || this.Size.equalsIgnoreCase("L") || this.Size.equalsIgnoreCase("XL") ){//Check Size select Not pass
         System.out.print("Please input Clothes you want to set : ");
         this.Index = Integer.parseInt(sc.nextLine()) - 1;
-        Clothes item = Stock.get(Index);
-        item.setSize(this.Size);
-        ShowValueInArrayList();
+        if (this.Index < -1 || this.Index > this.count - 1) {//Check index select Not pass
+                System.out.println("You input Index wrong please input one more time");
+                EditSize();
+        }
+            Clothes item = Stock.get(Index);
+            item.setSize(this.Size);
+            ShowValueInArrayList();
+            MainManu2();
+        } else {
+            System.out.println("You input wrong Size please input one more time");
+            EditSize();
+        }
     }
 
-    public void EditColor() {
-        sc.nextLine();
+    public void EditColor() {//Edit Color in Index
         System.out.print("Please input Color : ");
         this.Color = sc.nextLine();
         System.out.print("Please input Clothes you want to set : ");
         this.Index = Integer.parseInt(sc.nextLine()) - 1;
-        Clothes item = Stock.get(Index);
-        item.setColor(this.Color);
-        ShowValueInArrayList();
+        if (this.Index < 1 && this.Index > this.count) {//Check index select pass
+            Clothes item = Stock.get(Index);
+            item.setColor(this.Color);
+            ShowValueInArrayList();
+            MainManu2();
+        }else {//Check index select Not pass
+            System.out.println("You input wrong Index please input one more time");
+            EditColor();
+        }
     }
     
-    public void EditFabric() {
-        sc.nextLine();
+    public void EditFabric() {//Edit Fabric in Index
+        System.out.println("------Fabric------\n" +
+                "1.) Cotton +300Bath\n" +
+                "2.) Satin + 350Bath\n" +
+                "3.) Nylon + 400Bath\n");
+        try {
         System.out.print("Please input Fabric : ");
-        this.Fabric = sc.nextInt();
+        this.Fabric = Integer.parseInt(sc.nextLine());
+        if(this.Fabric >= 4 || this.Fabric <=  0){//Check Fabric select Not pass
+                System.out.println("You input Fabric wrong please input one more time");
+                EditFabric();
+            }
         System.out.print("Please input Clothes you want to set : ");
         this.Index = Integer.parseInt(sc.nextLine()) - 1;
-        Clothes item = Stock.get(Index);
-        item.setFabric(this.Fabric);
-        ShowValueInArrayList();
+        if (this.Index < -1 || this.Index > this.count - 1) {//Check index select Not pass
+                System.out.println("You input Index wrong please input one more time");
+                EditFabric();
+            }
+            Clothes item = Stock.get(Index);
+            item.setFabric(this.Fabric);
+            ShowValueInArrayList();
+            MainManu2();
+        } catch (Exception e) {
+            System.out.println("Please Don't Input String in this step");
+            EditFabric();
+        }
     }
 
-    public void Remove() {
-        System.out.print("Please input Clothes you want to set : ");
+    public void RemoveClothes() {//Remove Clothes in Stock
+        System.out.print("Please input Clothes you want to Remove : ");
+        try{
         this.Index = Integer.parseInt(sc.nextLine()) - 1;
-        Stock.remove(Index);
-        ShowValueInArrayList();
+        if(this.Index <= this.count - 1 && this.Index >= 0){//Check index pass
+            Stock.remove(Index);
+            this.count--;
+            ShowValueInArrayList();
+        } else {//Check index Not pass
+            System.out.println("You input index wrong please input one more time");
+            RemoveClothes();
+        }
+        } catch (Exception e) {
+            System.out.println("Please Don't Input String in this step");
+            RemoveClothes();
+        }
+    }
+
+    public void RemoveAll() {//Remove Clothes all in Stock
+        this.Stock.clear();
+        this.count = 0;
+        MainManu1();
+    }
+    
+    public void EditStyle() {//Edit Style in Index
+        System.out.println("------Style------\n" +
+                "1.)TShirt\n" +
+                "2.)Polo\n" +
+                "3.)Jacket\n");
+        try {
+            System.out.print("Please input Style : ");
+            this.Select = Integer.parseInt(sc.nextLine());
+            if (this.Select >= 4 || this.Select <= 0) {//Check style select Not pass
+                System.out.println("You input Style wrong please input one more time");
+                EditStyle();
+            }
+            System.out.print("Please input Clothes you want to set : ");
+            this.Index = Integer.parseInt(sc.nextLine()) - 1;
+            if (this.Index < -1 || this.Index > this.count - 1) {//Check index select Not pass
+                System.out.println("You input Index wrong please input one more time");
+                EditStyle();
+            }
+            Clothes item = Stock.get(Index);
+            item.setStyle(Fabric);
+            ShowValueInArrayList();
+            MainManu2();
+        } catch (Exception e) {
+            System.out.println("Please Don't Input String in this step");
+            EditStyle();
+        }
+    }
+    
+    private void CheckBill() {
+        for (int i = 0; i < Stock.size(); i++) {
+            this.FabricCharges = 0;
+            this.SizeCharges = 0;
+            Clothes Item = Stock.get(i);
+            switch (Item.getSize()) {
+                case "S":
+                    this.SizeCharges += 50;
+                    break;
+                case "M":
+                    this.SizeCharges += 100;
+                    break;
+                case "L":
+                    this.SizeCharges += 150;
+                    break;
+                case "XL":
+                    this.SizeCharges += 200;
+                    break;
+            }
+            switch (Item.getFabric()) {
+                case 1:
+                    this.FabricCharges += 300;
+                    break;
+                case 2:
+                    this.FabricCharges += 350;
+                    break;
+                case 3:
+                    this.FabricCharges += 400;
+                    break;
+            }
+            Item.setFabricCharges(this.FabricCharges);
+            Item.setSizeCharges(this.SizeCharges);
+            Item.setOverAllCharges(this.FabricCharges + this.SizeCharges);
+            this.Money = this.Money + this.FabricCharges + this.SizeCharges;
+        }
     }
 }
