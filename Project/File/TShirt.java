9cec47bff03bea4aec3816b13bb4eb24d3629d89
MilleMoo <118721832+MilleMoo@users.noreply.github.com>
2023-10-03 18:53:37 +0700

    Update TShirt.java

--- Project/File/TShirt.java
+++ Project/File/TShirt.java
@@ -1,10 +1,10 @@
 package File;
 
 public class TShirt extends Jacket {
-    public TShirt(int Style, String Size, String Color, int Fabric) {// นี่คือ constructor ของคลาส TShirt
-                                                                     // ที่ใช้สำหรับการสร้างอ็อบเจ็กต์ของคลาส TShirt
-        super(Style, Size, Color, Fabric);// ใช้ super เพื่อเรียก constructor ของคลาส Jacket
-                                          // ที่มีการรับพารามิเตอร์เหล่านี้ โดยทำให้คลาส TShirt
-                                          // สืบทอดคุณสมบัติและเมธอดจากคลาส Jacket
+    // นี่คือ constructor ของคลาส TShirt ที่ใช้สำหรับการสร้างอ็อบเจ็กต์ของคลาส TShirt
+    public TShirt(int Style, String Size, String Color, int Fabric) {
+        // ใช้ super เพื่อเรียก constructor ของคลาส Jacket ที่มีการรับพารามิเตอร์เหล่านี้ 
+        //โดยทำให้คลาส TShirt สืบทอดคุณสมบัติและเมธอดจากคลาส Jacket
+        super(Style, Size, Color, Fabric);
     }
 }
