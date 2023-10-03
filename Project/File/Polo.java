7dcee6250ec942d9955ee34bce5c7a0b2ee9056d
MilleMoo <118721832+MilleMoo@users.noreply.github.com>
2023-10-03 18:53:20 +0700

    Update Polo.java

--- Project/File/Polo.java
+++ Project/File/Polo.java
@@ -1,10 +1,10 @@
 package File;
 
 public class Polo extends Jacket{
-    public Polo(int Style, String Size, String Color, int Fabric) {// นี่คือ constructor ของคลาส Polo
-                                                                   // ที่ใช้สำหรับการสร้างอ็อบเจ็กต์ของคลาส Polo
-        super(Style, Size, Color, Fabric);// ใช้ super เพื่อเรียก constructor ของคลาส Jacket
-                                          // ที่มีการรับพารามิเตอร์เหล่านี้ โดยทำให้คลาส Polo
-                                          // สืบทอดคุณสมบัติและเมธอดจากคลาส
+    // นี่คือ constructor ของคลาส Polo ที่ใช้สำหรับการสร้างอ็อบเจ็กต์ของคลาส Polo
+    public Polo(int Style, String Size, String Color, int Fabric) {
+        // ใช้ super เพื่อเรียก constructor ของคลาส Jacket ที่มีการรับพารามิเตอร์เหล่านี้ 
+        //โดยทำให้คลาส Polo สืบทอดคุณสมบัติและเมธอดจากคลาส
+        super(Style, Size, Color, Fabric);
     }
 }
