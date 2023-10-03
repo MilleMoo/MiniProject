d643f8118a0d7f76beecca84f09e12b7755bb073
MilleMoo <118721832+MilleMoo@users.noreply.github.com>
2023-10-03 18:47:14 +0700

    Update Jacket.java

--- Project/File/Jacket.java
+++ Project/File/Jacket.java
@@ -9,8 +9,8 @@ public class Jacket implements Clothes{
     private int SizeCharges;
     private int OverAllCharges;
 
-    public Jacket(int Style, String Size, String Color, int Fabric) {// นี่คือ constructor ของคลาส Jacket
-                                                                     // ที่ใช้สำหรับการสร้างอ็อบเจ็กต์ของคลาส Jacket
+    // นี่คือ constructor ของคลาส Jacket ที่ใช้สำหรับการสร้างอ็อบเจ็กต์ของคลาส Jacket
+    public Jacket(int Style, String Size, String Color, int Fabric) {
         setColor(Color);
         setSize(Size);
         setFabric(Fabric);
@@ -18,76 +18,86 @@ public class Jacket implements Clothes{
     }
 
     @Override
-    public void setSize(String Size) {// ใช้สำหรับการกำหนดขนาดของเสื้อ (Size) เมื่อผู้ใช้ต้องการแก้ไขขนาดของเสื้อ
+    // ใช้สำหรับการกำหนดขนาดของเสื้อ (Size) เมื่อผู้ใช้ต้องการแก้ไขขนาดของเสื้อ
+    public void setSize(String Size) {
         this.Size = Size;
     }
 
     @Override
-    public void setColor(String Color) {// ใช้สำหรับการกำหนดสีของเสื้อ (Color) เมื่อผู้ใช้ต้องการแก้ไขสีของเสื้อ
+    // ใช้สำหรับการกำหนดสีของเสื้อ (Color) เมื่อผู้ใช้ต้องการแก้ไขสีของเสื้อ
+    public void setColor(String Color) {
         this.Color = Color;
     }
 
     @Override
-    public void setFabric(int fabric) {// ใช้สำหรับการกำหนดชนิดของผ้า (Fabric) เมื่อผู้ใช้ต้องการแก้ไขชนิดของผ้า
+    // ใช้สำหรับการกำหนดชนิดของผ้า (Fabric) เมื่อผู้ใช้ต้องการแก้ไขชนิดของผ้า
+    public void setFabric(int fabric) {
         this.Fabric = fabric;
     }
 
     @Override
-    public void setStyle(int Style) {// ใช้สำหรับการกำหนดรูปแบบของผ้า (Style) เมื่อผู้ใช้ต้องการแก้ไขรูปแบบของเสื้อ
+    // ใช้สำหรับการกำหนดรูปแบบของผ้า (Style) เมื่อผู้ใช้ต้องการแก้ไขรูปแบบของเสื้อ
+    public void setStyle(int Style) {
         this.Style = Style;
     }
 
     @Override
-    public String getSize() {// ใช้สำหรับการกำหนดค่าค่าใช้จ่ายเพิ่มเติมที่เกี่ยวข้องกับผ้า(Fabric Charges)
-                             // เมื่อผู้ใช้ต้องการแก้ไขค่านี้
+    // ใช้สำหรับการกำหนดค่าค่าใช้จ่ายเพิ่มเติมที่เกี่ยวข้องกับผ้า(Fabric Charges) เมื่อผู้ใช้ต้องการแก้ไขค่านี้
+    public String getSize() {
         return this.Size;
     }
 
     @Override
-    public String getColor() {// ใช้สำหรับการกำหนดค่าค่าใช้จ่ายเพิ่มเติมที่เกี่ยวข้องกับขนาด (Size Charges)
-                              // เมื่อผู้ใช้ต้องการแก้ไขค่านี้
+    // ใช้สำหรับการกำหนดค่าค่าใช้จ่ายเพิ่มเติมที่เกี่ยวข้องกับขนาด (Size Charges) เมื่อผู้ใช้ต้องการแก้ไขค่านี้
+    public String getColor() {
         return this.Color;
     }
 
     @Override
-    public int getFabric() {// ใช้สำหรับการกำหนดค่ารวมของค่าใช้จ่าย (Overall Charges)
-                            // เมื่อผู้ใช้ต้องการแก้ไขค่านี้
+    // ใช้สำหรับการกำหนดค่ารวมของค่าใช้จ่าย (Overall Charges) เมื่อผู้ใช้ต้องการแก้ไขค่านี้
+    public int getFabric() {
         return this.Fabric;
     }
     
     @Override
-    public int getStyle() {// ใช้สำหรับรับค่าขนาดของเสื้อ (Size) ที่ถูกตั้งค่า
+    // ใช้สำหรับรับค่าขนาดของเสื้อ (Size) ที่ถูกตั้งค่า
+    public int getStyle() {
         return this.Style;
     }
 
     @Override
-    public void setFabricCharges(int FabricCharges) {// ใช้สำหรับรับค่าสีของเสื้อ (Color) ที่ถูกตั้งค่า
+    // ใช้สำหรับรับค่าสีของเสื้อ (Color) ที่ถูกตั้งค่า
+    public void setFabricCharges(int FabricCharges) {
         this.FabricCharges = FabricCharges;
     }
 
     @Override
-    public void setSizeCharges(int SizeCharges) {// ใช้สำหรับรับค่าชนิดของผ้า (Fabric) ที่ถูกตั้งค่า
+    // ใช้สำหรับรับค่าชนิดของผ้า (Fabric) ที่ถูกตั้งค่า
+    public void setSizeCharges(int SizeCharges) {
         this.SizeCharges = SizeCharges;
     }
 
     @Override
-    public int getFabricCharges() {// ใช้สำหรับรับค่ารูปแบบของผ้า (Style) ที่ถูกตั้งค่า
+    // ใช้สำหรับรับค่ารูปแบบของผ้า (Style) ที่ถูกตั้งค่า
+    public int getFabricCharges() {
         return this.FabricCharges;
     }
 
     @Override
-    public int getSizeCharges() {// ใช้สำหรับรับค่าค่าใช้จ่ายเพิ่มเติมที่เกี่ยวข้องกับผ้า (Fabric Charges)ที่ถูกตั้งค่า
+    // ใช้สำหรับรับค่าค่าใช้จ่ายเพิ่มเติมที่เกี่ยวข้องกับผ้า (Fabric Charges)ที่ถูกตั้งค่า
+    public int getSizeCharges() {
         return this.SizeCharges;
     }
 
     @Override
-    public void setOverAllCharges(int OverAllCharges) {// ใช้สำหรับรับค่าค่าใช้จ่ายเพิ่มเติมที่เกี่ยวข้องกับขนาด (Size
-                                                       // Charges)ที่ถูกตั้งค่า
+    // ใช้สำหรับรับค่าค่าใช้จ่ายเพิ่มเติมที่เกี่ยวข้องกับขนาด (SizeCharges)ที่ถูกตั้งค่า
+    public void setOverAllCharges(int OverAllCharges) {
         this.OverAllCharges = OverAllCharges;
     }
 
     @Override
-    public int getOverAllCharges() {// ใช้สำหรับรับค่ารวมของค่าใช้จ่าย (Overall Charges) ที่ถูกตั้งค่า
+    // ใช้สำหรับรับค่ารวมของค่าใช้จ่าย (Overall Charges) ที่ถูกตั้งค่า
+    public int getOverAllCharges() {
         return OverAllCharges;
     }
 }
